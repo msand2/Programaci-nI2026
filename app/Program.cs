b38bbcd4b8a39b7e9b71d83cc6238a4267072486
Maiyovi Sandoval <tu-correo@ejemplo.com>
2026-02-17 21:31:15 -0600

    proyecto #

---
+++ app/Program.cs
@@ -0,0 +1,116 @@
+﻿/*Console.WriteLine(45);
+Console.WriteLine("informaticonfig");
+Console.WriteLine (50 + 60);
+
+//video 4
+int = 45;
+double = 45.5;
+bool = true/false;
+char = 'A';
+float = 45.5f;
+string = "asdasdasda";
+
+
+//video5
+string minombre = "informaticonfig";
+int edad = 45;
+bool activo = false;
+DateTime fecha = DateTime.Now;
+float precio = 45.5f;
+decimal descuento = 0.15m;
+
+Console.WriteLine (minombre);
+Console.WriteLine (edad);
+Console.WriteLine (fecha);
+Console.WriteLine (descuento);
+Console.WriteLine (precio);
+
+//video6
+string nombre = "ana";
+int edad = 30;
+
+Console.WriteLine ("nombre de usuario " + nombre);
+Console.WriteLine ("edad " + edad);
+
+Console.WriteLine ($"nombre de usuario {nombre}", edad en 10 años: {edad + 5}");
+
+console.writeline($"nombre de usuario {nombre}, edad: {edad}");
+
+//video8
+var numero = 10.20;
+Console.WriteLine(numero);
+numero = 3000;
+Console.WriteLine(numero);
+
+int numero2 = 150;
+Console.WriteLine("valor de constante " + numero2);
+numero2=80;
+Console.WriteLine (numero2);
+
+
+const double PI = 3.1416;   
+const string mensaje = "bienvenidos!"; 
+
+
+Console.ReadKey();
+
+//video9
+//casting implicito
+int valor = 200;
+int total = valor;
+Console.WriteLine ("implicito: " + total);
+
+//casting explicito
+double precio = 500.25;
+int descuento = (int)precio;
+Console.WriteLine("explicito: " + descuento);
+
+char letra = 'A';
+int codigoAscii = letra;
+Console.WriteLine(codigoAscii); 
+
+
+string palabra = "123456";
+int numero = Convert.ToInt32(palabra);
+Console.WriteLine (numero);
+
+string textodecimal = "150.60"; 
+double valor3 = double.Parse(textodecimal);*/
+
+//video10
+//operador de suma 
+int valor1 = 90, valor2=70, valor3=10;
+
+int total = valor1 + valor2;
+Console.WriteLine("total de suma: " + total);
+Console.WriteLine (valor1 + valor2);  
+
+//resta 
+int diferencia = valor1 - valor2;
+Console.WriteLine ("total de resta: " + diferencia);
+
+//multiplicacion 
+var producto = valor1 * valor2;
+Console.WriteLine ("producto: " + producto);
+
+//division
+var resultado = valor1 / valor3;
+Console.WriteLine ("resultado: " + resultado);
+
+//residuo
+var modulo = valor2 % valor3;
+Console.WriteLine ("residuo: " + modulo);
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
+
+
+
+
